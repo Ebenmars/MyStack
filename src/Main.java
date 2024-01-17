7d8f40daa463b304d2a59e5d7b38f76f05e7a95e
Ebenmars <76241509+Ebenmars@users.noreply.github.com>
2024-01-17 09:45:14 -0500

    Pop Method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       Stack myStack = new Stack(4);
+       myStack.push(3);
+        myStack.pop();
+        myStack.viewTop();
+        myStack.viewHeight();
+
+        myStack.printStack();
+
     }
 }
--- src/Stack.java
+++ src/Stack.java
@@ -20,19 +20,58 @@ public class Stack {
 
     public void push(int value){
         Node newNode = new Node(value);
-        if(top != null){
-            top.next = newNode;
-            top = newNode;
+
+        if(height == 0){
+           top = newNode;
+        }
+        else{
+           newNode.next = top;
+           top = newNode;
         }
         height++;
+    }
+
+    public Node pop(){
+        //store the value
+        Node temp = top;
+        //Node poppedNode = null;
+        if(height == 0){
+            return null;
+        }
+        else{
+            top = top.next;
+            temp.next = null;
+
+            //This works
+//            Node nextNode = top.next;
+//            poppedNode = nextNode;
+//            poppedNode = top;
+//            top = nextNode;
+        }
+
+        height--;
+        return temp;
     }
 
     public void printStack(){
         Node temp = top;
         while(temp != null){
-
+            System.out.print(temp.value);
+            if(temp != null){
+                System.out.print("-> ");
+            }
+            temp = temp.next;
         }
     }
+
+
+    public void viewTop(){
+        System.out.println("Top " + top.value);
+    }
+    public void viewHeight(){
+        System.out.println("Height " + height);
+    }
+
 
 
 
